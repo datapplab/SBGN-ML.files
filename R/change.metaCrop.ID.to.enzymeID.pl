df2b37771069bf03c45ffaf4e4caca24e9c540f0
Dong <xdong6@uncc.edu>
2018-10-25 15:02:10 -0400

    Added MetaCrop,MetaCyc

--- R/change.metaCrop.ID.to.enzymeID.pl
+++
@@ -1,2 +0,0 @@
-use strict;
-my $metaCropFiles = ``;
---
+++ R/change.sbgn.file.names.pl
@@ -0,0 +1,34 @@
+use strict;
+sub changeMetacycFileName{
+        my $MetacycFolder = "C:/xiaoxi/work/papers/SBGNview/package/SBGN-ML.files/data/SBGN/MetaCyc/";
+        my @MetaCycSbgnFiles = `ls $MetacycFolder `;
+        foreach my $metacycFile (@MetaCycSbgnFiles){
+                chomp $metacycFile;
+                my $oldFile = $metacycFile;
+                $metacycFile =~s/\.biopax\.sbgn//g;
+                print "\n\n $MetacycFolder/$metacycFile\n\n";
+                print "\n\n $MetacycFolder/$oldFile\n\n";
+                my $cpCommand = "mv -T $MetacycFolder/$oldFile $MetacycFolder/$metacycFile";
+                print "\n\n$cpCommand\n\n";
+                `$cpCommand`;
+        }
+}
+
+#changeMetacycFileName();
+
+sub changePathwayCommonsFileName{
+        my $PathwayCommonsFolder = "C:/xiaoxi/work/papers/SBGNview/package/SBGN-ML.files/data/SBGN/pathwayCommons/";
+        my @MetaCycSbgnFiles = `ls $PathwayCommonsFolder `;
+        foreach my $metacycFile (@MetaCycSbgnFiles){
+                chomp $metacycFile;
+                my $oldFile = $metacycFile;
+                $metacycFile =~s/\.xml//g;
+                print "\n\n $PathwayCommonsFolder/$metacycFile\n\n";
+                print "\n\n $PathwayCommonsFolder/$oldFile\n\n";
+                my $cpCommand = "mv -T $PathwayCommonsFolder/$oldFile $PathwayCommonsFolder/$metacycFile";
+                print "\n\n$cpCommand\n\n";
+                `$cpCommand`;
+        }
+}
+
+changePathwayCommonsFileName();
