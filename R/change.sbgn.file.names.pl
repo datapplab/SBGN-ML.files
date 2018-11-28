db54a969f9401368ed59cda481f99c4245c1dae1
Dong <xdong6@uncc.edu>
2018-11-28 14:07:53 -0500

    pathwayCommons simple_chemical using circle

--- R/change.sbgn.file.names.pl
+++ R/change.sbgn.file.names.pl
@@ -17,12 +17,13 @@ sub changeMetacycFileName{
 #changeMetacycFileName();
 
 sub changePathwayCommonsFileName{
-        my $PathwayCommonsFolder = "C:/xiaoxi/work/papers/SBGNview/package/SBGN-ML.files/data/SBGN/pathwayCommons/";
+        #my $PathwayCommonsFolder = "C:/xiaoxi/work/papers/SBGNview/package/SBGN-ML.files/data/SBGN/pathwayCommons/";
+        my $PathwayCommonsFolder = "C:/xiaoxi/work/output/layout.sbgn.svg.pathway.commons.pathways-sbgn.new.sbgn.with.svg";
         my @MetaCycSbgnFiles = `ls $PathwayCommonsFolder `;
         foreach my $metacycFile (@MetaCycSbgnFiles){
                 chomp $metacycFile;
                 my $oldFile = $metacycFile;
-                $metacycFile =~s/\.xml//g;
+                $metacycFile =~s/\.xml\.sbgn//g;
                 print "\n\n $PathwayCommonsFolder/$metacycFile\n\n";
                 print "\n\n $PathwayCommonsFolder/$oldFile\n\n";
                 my $cpCommand = "mv -T $PathwayCommonsFolder/$oldFile $PathwayCommonsFolder/$metacycFile";
