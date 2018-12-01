9698e37e40f792a81e9537fba31c801d9d32c48e
Dong <xdong6@uncc.edu>
2018-12-01 13:28:46 -0500

    added KO to metacyc mapping

--- R/generate.pathway.name.metaData.R
+++ R/generate.pathway.name.metaData.R
@@ -37,3 +37,39 @@ head(pathway.info.pre.generated.files)
 dim(pathway.info.pre.generated.files)
 save(pathway.info.pre.generated.files,file="./data/SBGN/pathway.info.pre.generated.files.RData")
 table(pathway.info.pre.generated.files[,"sub.database"])
+
+# add pathway description for metacyc
+metacyc.id.to.name = read.table("C:/xiaoxi/work/input/metaCyc/metaCyc/pathways.col.id-to-description.tsv.txt",stringsAsFactors=F,header=T,sep="\t",quote='"')
+head(metacyc.id.to.name)
+row.names(metacyc.id.to.name) = metacyc.id.to.name[,1]
+head(metacyc.id.to.name)
+dim(metacyc.id.to.name)
+library(SBGNview)
+data(pathway.info.pre.generated.files)
+head(pathway.info.pre.generated.files)
+
+dim(pathway.info.pre.generated.files[pathway.info.pre.generated.files[,"pathway.database"] == "MetaCyc",])
+head(pathway.info.pre.generated.files[pathway.info.pre.generated.files[,"pathway.database"] == "MetaCyc",])
+metacyc.ids = pathway.info.pre.generated.files[pathway.info.pre.generated.files[,"pathway.database"] == "MetaCyc","pathway.id"]
+head(metacyc.ids)
+metacyc.names = metacyc.id.to.name[metacyc.ids,2]
+head(metacyc.names)
+length(metacyc.names)
+pathway.info.pre.generated.files[pathway.info.pre.generated.files[,"pathway.database"] == "MetaCyc","DISPLAY_NAME"] = metacyc.names
+head(pathway.info.pre.generated.files[pathway.info.pre.generated.files[,"pathway.database"] == "MetaCyc",])
+save(pathway.info.pre.generated.files,file = "../SBGNview/data/pathway.info.pre.generated.files.RData")
+head(pathway.info.pre.generated.files)
+tail(pathway.info.pre.generated.files)
+dim(pathway.info.pre.generated.files)
+
+
+
+
+
+
+
+
+
+
+
+
