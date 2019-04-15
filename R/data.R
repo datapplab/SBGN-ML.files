0b8bec8da43b405538fc070136a432c5a8ed6ece
Dong <xdong6@uncc.edu>
2019-04-15 17:28:15 -0400

    Added IFNg dataset

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' IFNg KO mice dataset
+#' @description The normalized RNA-seq gene expression data from publication Greer, Renee L., Xiaoxi Dong, Ana Carolina F. Moraes, Ryszard A. Zielke, Gabriel R. Fernandes, Ekaterina Peremyslova, Stephany Vasquez-Perez et al. "Akkermansia muciniphila mediates negative effects of IFNg on glucose metabolism." Nature communications 7 (2016): 13329.
+#' @format SummarizedExperiment object
+#' @source \url{https://www.nature.com/articles/ncomms13329}
+'IFNg' 
+
+#' mapped ID pairs
+#' @format matrix
+#' @source \url{}
+'mapped.ids'
+
+#' Information of collected pathways
+#' @format data.frame
+#' @source \url{}
+'pathways.info'
+
+#' Number of pathways collected
+#' @format data.frame
+#' @source \url{}
+'pathways.stat'
+
+#' demo SBGNview object
+#' @format SBGNview object
+#' @source \url{}
+'SBGNview.obj'
